2f4be3717eb6b45ecc8860f5ba311b23f72e1ffc
Luke Turvey <luketurvey@gmail.com>
2014-07-18 16:47:04 -0700

    Update C4Shape+PolygonRegular.m
    -minor formatting changes (moving all brackets to match code style)
    -added comments explaining maths

--- C4/C4Shape+PolygonRegular.m
+++ C4/C4Shape+PolygonRegular.m
@@ -26,28 +26,29 @@ NSString* const C4ShapePolygonRegularType = @"polygon";
 @implementation C4Shape (PolygonRegular)
 
 + (instancetype)polygonRegularWithCenter:(CGPoint)center sideLength:(float)length numberOfSides:(int)numberOfSides eulerAnglePolyIsRotated:(float)euler{
+    //establish radius measurement to begin drawing polygon
     float radius = length/(2 * sin(DegreesToRadians(180.0f)/numberOfSides));
+    //limit minimum to create real polygons and limit maximum for memory considerations
     if (numberOfSides < 3)
         numberOfSides = 3;
     if (numberOfSides > 2048)
         numberOfSides = 2048;
     CGPoint pointArray[numberOfSides];
+    //set up the start point to begin drawing
     CGPoint start;
     CGPoint next;
     euler = euler + ((360.0f/numberOfSides)/2);
     start.x = center.x - radius * cos(DegreesToRadians(euler));
     start.y = center.y + radius * sin(DegreesToRadians(euler));
     pointArray[0] = start;
-    for (NSInteger i = 1; i < numberOfSides; i += 1)
-    {
-        if (i == 1 )
-        {
+    //draw the rest of the polygon looping back to start point
+    for (NSInteger i = 1; i < numberOfSides; i += 1){
+        if (i == 1 ){
             euler = euler + 180.0f - ((360.0f/numberOfSides)/2);
             next.x = start.x - length * cos(DegreesToRadians(euler));
             next.y = start.y + length * sin(DegreesToRadians(euler));
         }
-        else
-        {
+        else{
             euler = euler + (360.0f/numberOfSides);
             next.x = next.x - length * cos(DegreesToRadians(euler));
             next.y = next.y + length * sin(DegreesToRadians(euler));
