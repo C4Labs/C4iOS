e9408bd7a9bc61304465c89344a89f74499446f4
-- C4 -- <info@c4ios.com>
2014-10-31 10:32:32 -0700

    Replacing Rectangle.swift with Rect.swift

--- C4iOS/Rect.swift
+++ C4iOS/Rect.swift
@@ -11,27 +11,25 @@ import CoreGraphics
 
 public struct Rect : Equatable {
     public var origin: Point
-    public var width: Double
-    public var height: Double
+    public var size: Size
     
-    public init(_ x: Double, _ y: Double, _ width: Double, _ height: Double) {
+    public init(_ x: Double, _ y: Double, _ w: Double, _ h: Double) {
         origin = Point(x: x, y: y)
-        self.width = width
-        self.height = height
+        size = Size(w,h)
     }
 
     public var center: Point {
         get {
-            return Point(x: origin.x + width/2, y: origin.y + height/2)
+            return Point(x: origin.x + size.width/2, y: origin.y + size.height/2)
         }
         set {
-            origin.x = newValue.x - width/2
-            origin.y = newValue.y - height/2
+            origin.x = newValue.x - size.width/2
+            origin.y = newValue.y - size.height/2
         }
     }
 
     public func isZero() -> Bool {
-        return origin.isZero() && width == 0 && height == 0
+        return origin.isZero() && size.isZero()
     }
 
     public func contains(point: Point) -> Bool {
@@ -40,7 +38,7 @@ public struct Rect : Equatable {
 }
 
 public func == (lhs: Rect, rhs: Rect) -> Bool {
-    return lhs.origin == rhs.origin && lhs.width == rhs.width && lhs.height == rhs.height
+    return lhs.origin == rhs.origin && lhs.size == rhs.size
 }
 
 
@@ -50,14 +48,13 @@ public func == (lhs: Rect, rhs: Rect) -> Bool {
 public extension Rect {
     public init(_ rect: CGRect) {
         origin = Point(rect.origin)
-        width = Double(rect.size.width)
-        height = Double(rect.size.height)
+        size = Size(rect.size)
     }
 }
 
 public extension CGRect {
     public init(_ rect: Rect) {
         origin = CGPoint(rect.origin)
-        size = CGSizeMake(CGFloat(rect.width), CGFloat(rect.height))
+        size = CGSize(rect.size)
     }
 }
