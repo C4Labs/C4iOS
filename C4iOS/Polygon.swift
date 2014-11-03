f95008db7ae6b7aca55cacf7d92101d2ff4243e6
-- C4 -- <travis@c4ios.com>
2014-11-03 12:47:26 -0800

    Adding polygon class
    However, not sure if this should simply be the Shape class

---
+++ C4iOS/Polygon.swift
@@ -0,0 +1,14 @@
+//
+//  Polygon.swift
+//  C4iOS
+//
+//  Created by travis on 2014-11-03.
+//  Copyright (c) 2014 C4. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+import C4Core
+
+public class Polygon: Shape {
+}
