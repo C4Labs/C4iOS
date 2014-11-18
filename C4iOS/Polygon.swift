e469640d91e027a83ae7f827e22a4c33250c313d
-- C4 -- <travis@c4ios.com>
2014-11-17 16:36:46 -0800

    Adding functionality to Polygon

--- C4iOS/Polygon.swift
+++ C4iOS/Polygon.swift
@@ -11,4 +11,38 @@ import CoreGraphics
 import C4Core
 
 public class Polygon: Shape {
+    lazy internal var linePoints = [C4Point]()
+    convenience public init(points: [C4Point]) {
+        let count = points.count
+        var cgPoints = [CGPoint]()
+        for i in 0..<count {
+            cgPoints.append(CGPoint(points[i]))
+        }
+        self.init(frame: CGRectMakeFromPoints(cgPoints))
+        for i in 0..<points.count {
+            linePoints.append(points[i])
+        }
+        
+        updatePath()
+    }
+
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    required public init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    internal override func updatePath() {
+        if linePoints.count > 1 {
+        let path = C4Path()
+            path.moveToPoint(linePoints[0])
+            
+            for i in 1..<linePoints.count {
+                path.addLineToPoint(linePoints[i])
+            }
+            
+            shapeLayer.path = path.CGPath
+        }
+    }
 }
