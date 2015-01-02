3a26cc073a7d224c6100d5b683fb057e44ffcae6
-- C4 -- <info@c4ios.com>
2015-01-02 13:53:59 -0800

    Add TextShape
    Also add C4Font and C4EventSource as per: #226 #227

---
+++ C4UI/C4EventSource.swift
@@ -0,0 +1,43 @@
+// Copyright © 2014 C4
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions: The above copyright
+// notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+import Foundation
+import C4Core
+import UIKit
+
+public class C4EventSource : NSObject {
+    
+    //MARK: - EventSource
+    
+    internal func post(event: String) {
+        NSNotificationCenter.defaultCenter().postNotificationName(event, object: self)
+    }
+    
+    public func on(event notificationName: String, run executionBlock: Void -> Void) -> AnyObject {
+        let nc = NSNotificationCenter.defaultCenter()
+        return nc.addObserverForName(notificationName, object: self, queue: NSOperationQueue.currentQueue(), usingBlock: { notification in
+            executionBlock()
+        });
+    }
+    
+    public func cancel(observer: AnyObject) {
+        let nc = NSNotificationCenter.defaultCenter()
+        nc.removeObserver(observer)
+    }
+}
