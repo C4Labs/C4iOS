359f0b7ae672acce602f8ab9940c6f45cc8a99f5
-- C4 -- <info@c4ios.com>
2015-06-18 00:14:11 -0400

    Disables implicit animations during setup().

--- C4UI/C4CanvasController.swift
+++ C4UI/C4CanvasController.swift
@@ -22,7 +22,9 @@ import C4Core
 
 public class C4CanvasController : UIViewController {
     public override func viewDidLoad() {
+        C4ShapeLayer.disableActions = true
         self.setup()
+        C4ShapeLayer.disableActions = false
     }
 
     public func setup() {
