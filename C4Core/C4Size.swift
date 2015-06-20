1ddfc69299d18f97458f2c05335c6536e3d35c7f
-- C4 -- <info@c4ios.com>
2015-06-20 11:29:51 -0400

    Adds examples to C4Size documentation.

--- C4Core/C4Size.swift
+++ C4Core/C4Size.swift
@@ -26,6 +26,8 @@ public struct C4Size : Equatable, Comparable, Printable {
     
     /**
     Initializes a new C4Size with the dimensions {0,0}
+        
+        let s = C4Size()
     */
     public init() {
         width = 0
@@ -34,6 +36,8 @@ public struct C4Size : Equatable, Comparable, Printable {
     
     /**
     Initializes a new C4Size with the dimensions {width,height}
+    
+        let s = C4Size(5.2,5.2)
     */
     public init(_ width: Double, _ height: Double) {
         self.width = width
@@ -42,6 +46,8 @@ public struct C4Size : Equatable, Comparable, Printable {
     
     /**
     Initializes a new C4Size with the dimensions {width,height}, converting Int values to Double
+    
+        let s = C4Size(5,5)
     */
     public init(_ width: Int, _ height: Int) {
         self.width = Double(width)
@@ -50,6 +56,9 @@ public struct C4Size : Equatable, Comparable, Printable {
     
     /**
     Returns true if the dimensions of the receiver are {0,0}
+    
+        let s = C4Size()
+        s.isZero() //-> true
     */
     public func isZero() -> Bool {
         return width == 0 && height == 0
@@ -69,6 +78,10 @@ public struct C4Size : Equatable, Comparable, Printable {
 
 /**
 Returns true if the two source C4Size structs share identical dimensions
+
+    let s1 = C4Size()
+    let s2 = C4Size(1,1)
+    s1 == s2 //-> false
 */
 public func == (lhs: C4Size, rhs: C4Size) -> Bool {
     return lhs.width == rhs.width && lhs.height == rhs.height
@@ -76,6 +89,13 @@ public func == (lhs: C4Size, rhs: C4Size) -> Bool {
 
 /**
 Returns true if the left-hand size is bigger than the right-hand size
+    
+    let s1 = C4Size(3,4)
+    let s2 = C4Size(4,3)
+    let s3 = C4Size(2,2)
+
+    s1 > s2 //-> false
+    s2 > s3 //-> true
 */
 public func > (lhs: C4Size, rhs: C4Size) -> Bool {
     return lhs.width * lhs.height > rhs.width * rhs.height
@@ -83,6 +103,14 @@ public func > (lhs: C4Size, rhs: C4Size) -> Bool {
 
 /**
 Returns true if the left-hand size is smaller than the right-hand size
+
+    let s1 = C4Size(3,4)
+    let s2 = C4Size(4,3)
+    let s3 = C4Size(2,2)
+
+    s1 < s2 //-> false
+    s2 < s3 //-> false
+
 */
 public func < (lhs: C4Size, rhs: C4Size) -> Bool {
     return lhs.width * lhs.height < rhs.width * rhs.height
@@ -90,6 +118,13 @@ public func < (lhs: C4Size, rhs: C4Size) -> Bool {
 
 /**
 Returns true if the left-hand size is greater than or equal to the right-hand size
+
+    let s1 = C4Size(3,4)
+    let s2 = C4Size(4,3)
+    let s3 = C4Size(2,2)
+
+    s1 >= s2 //-> true
+    s2 >= s3 //-> true
 */
 public func >= (lhs: C4Size, rhs: C4Size) -> Bool {
     return lhs.width * lhs.height >= rhs.width * rhs.height
@@ -97,6 +132,13 @@ public func >= (lhs: C4Size, rhs: C4Size) -> Bool {
 
 /**
 Returns true if the left-hand size is smaller than or equal to the right-hand size
+
+    let s1 = C4Size(3,4)
+    let s2 = C4Size(4,3)
+    let s3 = C4Size(2,2)
+
+    s1 <= s2 //-> true
+    s2 <= s3 //-> false
 */
 public func <= (lhs: C4Size, rhs: C4Size) -> Bool {
     return lhs.width * lhs.height <= rhs.width * rhs.height
