c0446438d39a6451a84e62d71a04efcdde0ded70
-- C4 -- <info@c4ios.com>
2015-06-22 11:50:36 -0400

    Initializes a line with only 2 points
    Was previously initializing lines as if they were polygons.

--- C4UI/C4Line.swift
+++ C4UI/C4Line.swift
@@ -56,4 +56,38 @@ public class C4Line: C4Polygon {
             path = p
         }
     }
+
+
+    /**
+    Initializes a new C4Polygon using the specified array of points.
+
+    Protects against trying to create a polygon with only 1 point (i.e. requires 2 points).
+    Trims point array if count > 2.
+
+    let a = C4Point(100,100)
+    let b = C4Point(200,200)
+
+    let l = C4Line([a,b])
+
+    :param: points An array of C4Point structs.
+    */
+    convenience public init(var _ points: [C4Point]) {
+
+        if points.count > 2 {
+            do {
+                points.removeLast()
+            } while points.count > 2
+        }
+
+        self.init(frame: C4Rect(points))
+        var path = C4Path()
+        self.points = points
+        path.moveToPoint(points[0])
+        for i in 1..<points.count {
+            path.addLineToPoint(points[i])
+        }
+        self.path = path
+        adjustToFitPath()
+    }
+
 }
