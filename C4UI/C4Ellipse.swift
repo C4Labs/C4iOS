e765d89b741450f1b37dde57e518f7990f03c517
-- C4 -- <info@c4ios.com>
2015-06-22 11:07:30 -0400

    Adds example to C4Ellipse

--- C4UI/C4Ellipse.swift
+++ C4UI/C4Ellipse.swift
@@ -25,6 +25,9 @@ public class C4Ellipse: C4Shape {
     /**
     Creates an ellipse.
     
+        let r = C4Rect(0,0,100,200)
+        let e = C4Ellipse(frame: r)
+        
     :param: frame The frame within which to draw an ellipse that touches each of the four sides of the frame.
     */
     convenience public init(frame: C4Rect) {
