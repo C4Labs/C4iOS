c17d0e3a861e64703511b8c9f6a3d286d2d4ea34
-- C4 -- <info@c4ios.com>
2015-09-24 22:16:41 -0700

    Gradients can now handle multiple colors
    Includes asserts that check input values for initializer and variables.

--- C4/UI/C4Gradient.swift
+++ C4/UI/C4Gradient.swift
@@ -55,6 +55,7 @@ public class C4Gradient : C4View {
     }
     
     public convenience init(frame: C4Rect, colors: [C4Color], locations: [Double]) {
+        assert(colors.count == locations.count, "colors and locations need to have the same number of elements")
         self.init(frame: frame)
         self.colors = colors
         self.locations = locations
@@ -63,15 +64,20 @@ public class C4Gradient : C4View {
     public var colors : [C4Color] {
         get {
             if let cgcolors = gradientLayer.colors as? [CGColorRef] {
-                return [C4Color(cgcolors[0]),C4Color(cgcolors[1])]
+                var array = [C4Color]()
+                for c in cgcolors {
+                    array.append(C4Color(c))
+                }
+                return array
             }
             return [C4Blue,C4Pink]
         } set {
-            assert(newValue.count == 2)
-            if let c1 : C4Color = newValue.first,
-                let c2 : C4Color = newValue.last {
-                    self.gradientLayer.colors = [c1.CGColor,c2.CGColor]
+            assert(newValue.count >= 2, "colors must have at least 2 elements")
+            var cgcolors = [CGColorRef]()
+            for c in newValue {
+                cgcolors.append(c.CGColor)
             }
+            self.gradientLayer.colors = cgcolors
         }
     }
     
