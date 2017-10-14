b8cb348d881b1a37716656eca83810cd57089eab
Alejandro Isaza <al@isaza.ca>
2017-10-14 11:58:13 -0700

    Fix compiler error on iOS target

--- C4/UI/View+Gestures.swift
+++ C4/UI/View+Gestures.swift
@@ -17,7 +17,7 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 
-import Foundation
+import UIKit
 
 extension View {
     /// Adds a tap gesture recognizer to the receiver's view.
